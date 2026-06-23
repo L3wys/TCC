6b406a2e9da08cc643ed68f45128990bf86f08fc
turi-afs <rm98236@estudante.fieb.edu.br>
2026-06-23 19:11:21 +0000

    Quase tudo pronto guys

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+
 import PagMod from './paginassaudezen/PagMod'
 
 function App() {
--- src/paginassaudezen/PagMod.jsx
+++ src/paginassaudezen/PagMod.jsx
@@ -1,11 +1,103 @@
-
+import React from "react";
+import './pagmod.css'
 
 const PagMod = () => {
     return(
-      <div>
-        <h1>Oi</h1>
+    
+<>
+ 
+  <div className="tes">
+    <div className="faixa"></div>
+    <img style={{ marginLeft: '-5px' }} src="imagem/image 5.png" />
+    <nav>
+      <ul>
+        <li><a href=""></a>MENU</li>
+        <li><a href=""></a>DICAS DIÁRIAS</li>
+        <li><a href=""></a>SAÚDE A-Z</li>
+        <li><a href=""></a>SOBRE</li>
+      </ul>
+    </nav>
+ 
+ 
+ 
+    <button style={{ backgroundColor: 'rgb(28, 28, 122)', width: '150px', marginLeft: '25px', marginRight: '-35px' }}
+      className="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions"
+      aria-controls="offcanvasWithBothOptions"><a className="icon-link" href="#">
+        <svg xmlns="http://www.w3.org/2000/svg" className="bi" viewBox="0 0 1 1" aria-hidden="true">
+          <img style={{ width: '20px', marginLeft: '-25px' }} src="/imagem/lupa 2.png"/>
+        </svg>
+        Pesquisar
+      </a></button>
+ 
+    <div className="offcanvas offcanvas-start" data-bs-scroll="true" tabIndex="-1" id="offcanvasWithBothOptions"
+      aria-labelledby="offcanvasWithBothOptionsLabel">
+      <div style={{ backgroundColor: 'rgb(81, 187, 187)' }} className="offcanvas-header">
+        <h5 className="offcanvas-title" id="offcanvasWithBothOptionsLabel">Pesquise sobre aqui</h5>
+        <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
-    )
+      <nav className="navbar bg-body-tertiary">
+        <form className="d-flex" role="search">
+          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+          <button className="btn btn-light" type="submit">Search</button>
+        </form>
+      </nav>
+    </div>
+    <button className="con">Fale Conosco</button>
+    <button style={{ marginLeft: 'auto' }} className="con"><a href="loginpage.html"> Criar conta/entrar</a></button>
+    <div className="faixa1"></div>
+  </div><br />
+  <center>
+  <img style={{ height: 'auto', width: '900px' }} src="/szLOGO.png"/>
+  </center>
+  <br />
+  <div id="carouselExampleCaptions" className="carousel slide">
+    <div className="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active"
+        aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
+        aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
+        aria-label="Slide 3"></button>
+    </div>
+    <div className="carousel-inner">
+      <div className="carousel-item active">
+        <img src="/imagem/416a9bcd-f814-4948-b910-1c37c6f16c74 5.jpeg" className="d-block w-100"
+          style={{ height: '500px', objectFit: 'cover' }} alt=""/>
+        <div className="carousel-caption d-none d-md-block">
+          <h5>First slide label</h5>
+          <p>Some representative placeholder content for the first slide.</p>
+        </div>
+      </div>
+      <div className="carousel-item">
+        <img src="/imagem/bebe-2 5.webp" className="d-block w-100" style={{ height: '500px', objectFit: 'cover' }} alt=""/>
+        <div className="carousel-caption d-none d-md-block">
+          <h5>Second slide label</h5>
+          <p>Some representative placeholder content for the second slide.</p>
+        </div>
+      </div>
+      <div className="carousel-item">
+        <img src="/imagem/szLOGO 7.png" className="d-block w-100" style={{ height: '500px', objectFit: 'cover' }} alt="..."/>
+        <div className="carousel-caption d-none d-md-block">
+          <h5>Third slide label</h5>
+          <p>Some representative placeholder content for the third slide.</p>
+        </div>
+      </div>
+    </div>
+    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+      <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span className="visually-hidden">Previous</span>
+    </button>
+    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+      <span className="carousel-control-next-icon" aria-hidden="true"></span>
+      <span className="visually-hidden">Next</span>
+    </button>
+</div>
+</>
+)
+
 }
 
-export default PagMod
+
+
+
+export default PagMod;
