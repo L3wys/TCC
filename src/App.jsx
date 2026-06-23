7626128d034411a2d8645d1d9986a23719c207d0
turi-afs <rm98236@estudante.fieb.edu.br>
2026-06-23 15:21:27 +0000

    CONSEGUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
+import PagMod from './paginassaudezen/PagMod'
 
 function App() {
 
   return (
-    <pagmod/>
+    <>
+      <PagMod/>
+    </>
   )
 }
 
--- src/paginassaudezen/pagmod.jsx
+++
@@ -1,9 +0,0 @@
-const pagmod = () => {
-    return(
-        <div className="container">
-            <h1>Oi</h1>
-        </div>
-    )
-}
-
-export default pagmode
