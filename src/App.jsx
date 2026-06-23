2a03035965e8d99d665183df7c029aad2551232b
turi-afs <rm98236@estudante.fieb.edu.br>
2026-06-23 15:06:07 +0000

    Testando o html

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div>
-    <h1>Oi</h1>
-    <h1>Outro teste</h1>
-    </div>
+    <pagmod/>
   )
 }
 
